102ca537dd8629b7b8a5280175106967dc125b4d
‘SasaZuza’ <sasazuzazule@gmail.com>
2018-09-30 20:49:51 +0200

    Added files

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function() {
+
+  'use strict';
+
+// Configuring the quote grids funcionality 
+
+  $('.services-carousel').owlCarousel({
+      autoplay: true,
+      loop: true,
+      margin: 20,
+      dots: true,
+      nav: false,
+      responsiveClass: true,
+      responsive: { 0: { items: 1 }, 768: { items: 2 }, 900: { items: 4 } }
+    });
+    
+});
